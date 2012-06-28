e4602384f582801075c032b37816bb1e92d63e46
George Vacariuc <gvacariuc@tendrilinc.com>
2012-06-28 16:08:44 -0600

    client side exception deserializer

--- src/main/scala/com/twitter/finagle/protobuf/rpc/impl/RpcChannelImpl.scala
+++ src/main/scala/com/twitter/finagle/protobuf/rpc/impl/RpcChannelImpl.scala
@@ -5,8 +5,9 @@ import com.twitter.finagle.protobuf.rpc.RpcControllerWithOnFailureCallback
 import com.twitter.finagle.protobuf.rpc.channel.ProtoBufCodec
 import com.twitter.finagle.ChannelClosedException
 import com.twitter.finagle.protobuf.rpc.Util
+import com.twitter.finagle.protobuf.rpc.ExceptionResponseHandler
 
-class RpcChannelImpl(cb: ClientBuilder[(String, Message), (String, Message), Any, Any, Any], s: Service, executorService: ExecutorService) extends RpcChannel {
+class RpcChannelImpl(cb: ClientBuilder[(String, Message), (String, Message), Any, Any, Any], s: Service, handler: ExceptionResponseHandler[Message], executorService: ExecutorService) extends RpcChannel {
 
   private val log = LoggerFactory.getLogger(getClass)
 
@@ -19,19 +20,35 @@ class RpcChannelImpl(cb: ClientBuilder[(String, Message), (String, Message), Any
   def callMethod(m: MethodDescriptor, controller: RpcController,
     request: Message, responsePrototype: Message,
     done: RpcCallback[Message]): Unit = {
+    // retries is a workaround for ChannelClosedException raised when servers shutdown.
+    val retries = 3
+
+    callMethod(m, controller, request, responsePrototype, done, retries)
+  }
+
+  def callMethod(m: MethodDescriptor, controller: RpcController,
+    request: Message, responsePrototype: Message,
+    done: RpcCallback[Message], retries: Int): Unit = {
 
     Util.log("Request", m.getName(), request)
     val req = (m.getName(), request)
 
     client(req) onSuccess { result =>
       Util.log("Response", m.getName(), result._2)
-      futurePool({ done.run(result._2) })
+      futurePool({ handle(done, controller, result._2) })
     } onFailure { e =>
       log.warn("Failed. ", e)
       controller.asInstanceOf[RpcControllerWithOnFailureCallback].setFailed(e)
     }
   }
 
+  def handle(done: RpcCallback[Message], controller: RpcController, m: Message) {
+    if (handler.canHandle(m)) {
+      controller.asInstanceOf[RpcControllerWithOnFailureCallback].setFailed(handler.handle(m))
+    } else {
+      done.run(m)
+    }
+  }
   def release() {
      client.close()
   }
